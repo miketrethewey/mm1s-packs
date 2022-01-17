b0ea379b2bbba938fb3ebd2ea37f23a482783240
Mike A. Trethewey <miketrethewey@gmail.com>
2022-01-17 15:10:59 -0800

    Import json

--- resources/ci/common/prepare_repository.py
+++ resources/ci/common/prepare_repository.py
@@ -1,5 +1,6 @@
 import common
 import distutils.dir_util     # for copying trees
+import json
 import os                     # for env vars
 import stat                   # for file stats
 import subprocess             # do stuff at the shell level
